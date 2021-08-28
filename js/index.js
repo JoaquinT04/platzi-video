56514a375a9e54c7b02dd00b746231a28daa94d5
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2021-08-27 22:27:52 -0300

    Agregando el plugin AutoPause y poniendo en practica Service Workers

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
 const video = document.querySelector('.video__movie');
 const buttonPlay = document.querySelector('.video__playButton');
@@ -9,9 +10,17 @@ const player = new MediaPlayer({
 	video: video,
 	plugins: [
 		new AutoPlay(),
+		new AutoPause(),
 	],
 });
 
 buttonPlay.onclick = () => player.playPause();
 
 buttonMute.onclick = () => player.muteUnmute();
+
+
+if ('serviceWorker' in navigator){
+	navigator.serviceWorker.register('../sw.js').catch(error => {
+		console.log(error.message);
+	})
+}
